388eba9e6cf523269fda9d716b0568fcd620699f
Bruno Genero <generobruno@gmail.com>
2023-10-07 10:44:45 -0300

    uart test bench - assertion

--- sources/UART_Test/sims/uart_tb.v
+++ sources/UART_Test/sims/uart_tb.v
@@ -14,8 +14,8 @@ module uart_tb();
     wire o_tx_full, o_rx_empty;
     wire [7:0] o_r_data;
     reg [7:0] data_to_send; // Data to be sent
-    reg [7:0] data_to_send2;
     integer bit_count;
+    integer received_data_mismatch;
 
     // Instantiate the UART module
     uart_top #(
@@ -83,28 +83,43 @@ module uart_tb();
     // Test cases
     initial
     begin
-        // Initialize testbench signals
-        i_rd_uart = 1'b0;
-        i_rx = 1'b1;
-        data_to_send = 8'b10101010; // Data to be sent
-        bit_count = 0;
+    // Initialize testbench signals
+    i_rd_uart = 1'b0;
+    i_rx = 1'b1;
+    data_to_send = 8'b01010101; // Data to be sent
+    bit_count = 0;
 
-        @(negedge i_reset); // Wait for reset to deassert
+    @(negedge i_reset); // Wait for reset to deassert
 
-        //! Test: Send all data 
-        UART_RECEIVE_BYTE(data_to_send);
+    //! Test: Send all data
+    UART_RECEIVE_BYTE(data_to_send);
 
-        // Test Case: Read received data 
-        while (o_rx_empty != 1) begin 
-            i_rd_uart = 1'b1;   // Read FIFO
-            $display("Received bits: %b", o_r_data);
-            @(negedge i_clk);   // Assert i_rd_signal for 1 clk cycle to remove word
-            i_rd_uart = 1'b0;
-            @(negedge i_clk);
+    // Test Case: Read received data
+    received_data_mismatch = 0;
+
+    while ((o_rx_empty != 1) && (received_data_mismatch != 1)) begin
+        i_rd_uart = 1'b1;   // Read FIFO
+        $display("Received bits: %b", o_r_data);
+
+        // Compare received data with sent data
+        if (o_r_data !== data_to_send) begin
+            $display("Data Mismatch! Received data does not match sent data.");
+            received_data_mismatch = 1;
         end
 
-        // Stop simulation
-        $stop;
+        @(negedge i_clk);   // Assert i_rd_signal for 1 clk cycle to remove word
+        i_rd_uart = 1'b0;
+        @(negedge i_clk);
     end
+
+    if (received_data_mismatch == 0)
+        $display("\nAll received data matches sent data. Test Passed!");
+    else
+        $display("\nFailed Receiving Data.");
+
+    // Stop simulation
+    $stop;
+    end
+
 
 endmodule
