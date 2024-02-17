58753a4fb3f9b01cad3117b02f36a760a1fb2855
Bruno Genero <generobruno@gmail.com>
2024-02-17 10:17:26 -0300

    ID instantiation and corrections

--- sources/PIPELINE_Test/PIPELINE/pipeline.v
+++ sources/PIPELINE_Test/PIPELINE/pipeline.v
@@ -41,7 +41,7 @@ module pipeline
         // Input Control Lines
         .i_pc_src_D(), .i_jump_D(), .i_jump_sel_D(), .i_stall_pc_HD(),
         // Outputs
-        .o_npc_F(o_npc_F), .o_branch_delay_slot_F(o_branch_delay_slot_F), .o_instruction_F(o_instruction_F)
+        .o_npc_F(npc_F), .o_branch_delay_slot_F(branch_delay_slot_F), .o_instruction_F(instruction_F)
         );
 
     /**
@@ -52,9 +52,9 @@ module pipeline
         // Sync Signals
         .i_clk(i_clk), .i_reset(i_reset), .i_enable(), //TODO En reset tambien van PCSrc, Jump y JumpSel
         // Inputs
-        .i_instruction(o_instruction_F), .i_npc(o_npc_F), .i_bds(o_branch_delay_slot_F),
+        .i_instruction(instruction_F), .i_npc(npc_F), .i_bds(branch_delay_slot_F),
         // Outputs
-        .o_instruction(i_instruction_D), .o_npc(i_npc_D), .o_bds()
+        .o_instruction(instruction_D), .o_npc(npc_D), .o_bds(bds)
         );
 
     /**
@@ -62,14 +62,16 @@ module pipeline
     **/
     ID #(.INST_SZ(INST_SZ), .REG_SZ(REG_SZ), .FORW_EQ(FORW_EQ)) InstructionDecode
         (
+        // Sync Signals
+        .i_clk(i_clk), .i_reset(i_reset),
         // Inputs
-        .i_instruction_D(i_instruction_D), .i_npc_D(i_npc_D),
+        .i_instruction_D(instruction_D), .i_npc_D(npc_D),
         // Input Control Lines
-        .i_forward_eq_a_FU(), .i_forward_eq_b_FU(), .i_alu_result_M(),
-        .i_branch_MC(), .i_equal_MC(),
+        .i_forward_eq_a_FU(forward_eq_a_FU), .i_forward_eq_b_FU(forward_eq_b_FU), .i_alu_result_M(),
+        .i_branch_MC(branch_MC), .i_equal_MC(equal_MC),
         .i_reg_write_M(), .i_write_register_D(), .i_write_data_D(),
         // Outputs
-        .o_jump_addr_D(), .o_read_data_1_D(), .o_read_data_2_D(),
+        .o_jump_addr_D(), .o_branch_addr_D(), .o_read_data_1_D(), .o_read_data_2_D(),
         .o_pc_src_D(), .o_instr_op_D(), .o_instr_funct_D(), .o_instr_index_D(),
         .o_instr_imm_D(), .o_instr_rs_D(), .o_instr_rt_D(), .o_instr_rd_D()
         );
@@ -82,9 +84,9 @@ module pipeline
         // Sync Signals
         .i_clk(i_clk), .i_reset(i_reset), .i_enable(),
         // Input Control Lines //TODO Agregar SXL/SXLV Control Line
-        .i_alu_src(), .i_alu_op(), .i_reg_dst(), .i_jal_sel(), 
-        .i_jump(), .i_jump_sel(), .i_mem_read(), .i_mem_write(), 
-        .i_reg_write(), .i_mem_to_reg(), .i_bds_sel(),
+        .i_alu_src(alu_src_MC), .i_alu_op(alu_op_MC), .i_reg_dst(reg_dst_MC), .i_jal_sel(jal_sel_MC), 
+        .i_jump(jump_MC), .i_jump_sel(jump_sel_MC), .i_mem_read(mem_read_MC), .i_mem_write(mem_write_MC), 
+        .i_reg_write(reg_write_MC), .i_mem_to_reg(mem_to_reg_MC), .i_bds_sel(bds_sel_MC),
         // Inputs
         .i_read_data_1(), .i_read_data_2(),
         .i_instr_imm(), .i_instr_rt(), .i_instr_rd(), .i_instr_rs(),
@@ -109,8 +111,8 @@ module pipeline
         .i_instr_imm_D(), .i_instr_rs_D(),
         .i_instr_rt_D(), .i_instr_rd_D(),
         // Input Control Lines //TODO Agregar SXL/SXLV Control Line
-        .i_alu_src_MC(), .i_reg_dst_MC(), .i_jal_sel_MC(),
-        .i_alu_op_MC(), .i_forward_a_FU(), .i_forward_b_FU(),
+        .i_alu_src_MC(alu_src_MC), .i_reg_dst_MC(reg_dst_MC), .i_jal_sel_MC(jal_sel_MC),
+        .i_alu_op_MC(alu_op_MC), .i_forward_a_FU(forward_a_FU), .i_forward_b_FU(forward_b_FU),
         // Outputs
         .o_jump_addr_D(), .o_read_data_1_D(), .o_read_data_2_D(),
         .o_pc_src_D(), .o_instr_op_D(), .o_instr_funct_D(), .o_instr_index_D(),
@@ -192,22 +194,22 @@ module pipeline
     MainControlUnit #(.OPCODE_SZ(OPCODE_SZ), .FUNCT_SZ(FUNCT_SZ)) MainControlUnit
         (
             // Inputs
-            .i_instr_op_D(),
-            .i_instr_funct_D(),
+            .i_instr_op_D(instruction_D[31 : 26]),
+            .i_instr_funct_D(instruction_D[5 : 0]),
             // Output Control Lines
-            .o_alu_op_MC(),
-            .o_reg_dst_MC(),
-            .o_jal_sel_MC(),
-            .o_alu_src_MC(),
-            .o_branch_MC(),
-            .o_equal_MC(),
-            .o_mem_read_MC(),
-            .o_mem_write_MC(),
-            .o_jump_MC(),
-            .o_jump_sel_MC(),
-            .o_reg_write_MC(),
-            .o_bds_sel_MC(),
-            .o_mem_to_reg_MC()
+            .o_alu_op_MC(alu_op_MC),
+            .o_reg_dst_MC(reg_dst_MC),
+            .o_jal_sel_MC(jal_sel_MC),
+            .o_alu_src_MC(alu_src_MC),
+            .o_branch_MC(branch_MC),
+            .o_equal_MC(equal_MC),
+            .o_mem_read_MC(mem_read_MC),
+            .o_mem_write_MC(mem_write_MC),
+            .o_jump_MC(jump_MC),
+            .o_jump_sel_MC(jump_sel_MC),
+            .o_reg_write_MC(reg_write_MC),
+            .o_bds_sel_MC(bds_sel_MC),
+            .o_mem_to_reg_MC(mem_to_reg_MC)
         );
 
     //  Hazard Detection Unit
@@ -242,10 +244,10 @@ module pipeline
             .i_reg_write_M(),
             .i_reg_write_W(),
             // Output Control Lines
-            .o_forward_eq_a_FU(),
-            .o_forward_eq_b_FU(),
-            .o_forward_a_FU(),
-            .o_forward_b_FU(),
+            .o_forward_eq_a_FU(forward_eq_a_FU),
+            .o_forward_eq_b_FU(forward_eq_b_FU),
+            .o_forward_a_FU(forward_a_FU),
+            .o_forward_b_FU(forward_b_FU),
 
         );
 
@@ -256,7 +258,7 @@ module pipeline
             .i_instr_funct_E(),            
             .i_alu_op_MC(),                 
             // Output Control Lines
-            .o_alu_sel_AC(),
+            .o_alu_sel_AC(alu_sel_AC),
         );
 
 
--- sources/PIPELINE_Test/PIPELINE/src/Mod_ID/ID.v
+++ sources/PIPELINE_Test/PIPELINE/src/Mod_ID/ID.v
@@ -11,6 +11,8 @@ module ID
     )
     (
         // Inputs
+        input                           i_clk,                      // Clock
+        input                           i_reset,                    // Reset
         input [INST_SZ-1 : 0]           i_instruction_D,            // Instruction Fetched
         input [INST_SZ-1 : 0]           i_npc_D,                    // NPC
         input [FORW_EQ-1 : 0]           i_forward_eq_a_FU,          // Forwarding Eq A Control Line
@@ -23,6 +25,7 @@ module ID
         input [INST_SZ-1 : 0]           i_write_data_D,             // Write Data
         // Outputs
         output [INST_SZ-1 : 0]          o_jump_addr_D,              // Jump Address
+        output [INST_SZ-1 : 0]          o_branch_addr_D,            // Branch Address
         output [INST_SZ-1 : 0]          o_read_data_1_D,            // Read Data 1 (from reg mem)
         output [INST_SZ-1 : 0]          o_read_data_2_D,            // Read Data 2 (from reg mem)
         output                          o_pc_src_D,                 // PCSrc Control Line
@@ -35,6 +38,56 @@ module ID
         output [4 : 0]                  o_instr_rd_D                // Instruction RD (instr[15:11])
     );
 
-    // TODO Instanciar: Branch_Addr_Adder, Register_Memory, Comparator, MPX (x2)
+    //! Signal Declaration
+    wire xnor_result;
+    wire comparison;
+    wire [INST_SZ-1:0] extended_imm;
+    wire [INST_SZ-1:0] shifted_imm;
+
+    //! Instantiations
+    mpx_2to1 #(.N(INST_SZ)) forw_eq_a_mpx
+        (.input_a(read_data_1), .input_b(i_alu_result_M),
+        .i_select(i_forward_eq_a_FU),
+        .o_output(o_read_data_1_D));
+
+    mpx_2to1 #(.N(INST_SZ)) forw_eq_b_mpx
+        (.input_a(read_data_2), .input_b(i_alu_result_M),
+        .i_select(i_forward_eq_b_FU),
+        .o_output(o_read_data_2_D));
+
+    comparator #(.INST_SZ(INST_SZ)) comparator
+        (.i_read_data_1(o_read_data_1_D),
+        .i_read_data_2(o_read_data_2_D),
+        .o_comparison(comparison));
+
+    register_mem #(.B(INST_SZ), .W()) register_mem
+        (.i_clk(i_clk), //TODO Reset?
+        .i_reg_write_MC(i_reg_write_W),
+        .i_read_reg_1(i_instruction_D[25 : 21]), .i_read_reg_2(i_instruction_D[20 : 16]),
+        .i_write_register(i_write_register_D), .i_write_data(i_write_data_D),
+        .o_read_data_1(read_data_1), .o_read_data_2(read_data_2));
+
+    //! Assignments
+    assign o_instr_op_D         =      i_instruction_D[31 : 26]; 
+    assign o_instr_funct_D      =      i_instruction_D[5 : 0]; 
+    assign o_instr_index_D      =      i_instruction_D[25 : 0]; 
+    assign o_instr_imm_D        =      i_instruction_D[15 : 0]; 
+    assign o_instr_rs_D         =      i_instruction_D[25 : 21]; 
+    assign o_instr_rt_D         =      i_instruction_D[20 : 16]; 
+    assign o_instr_rd_D         =      i_instruction_D[15 : 11]; 
+
+    // Shift instruction index to get jump address
+    assign o_jump_addr_D = {i_npc_D[31:28], o_instr_index_D << 2};
+
+    // Decide if branch or not
+    assign xnor_result = ~(comparison ^ i_equal_MC);
+    assign o_pc_src_D = xnor_result & i_branch_MC;
+
+    // Sign extension of o_instr_imm_D to INST_SZ length
+    assign extended_imm = {{(INST_SZ-16){o_instr_imm_D[15]}}, o_instr_imm_D};
+    // Left shift extended immediate by 2 bits
+    assign shifted_imm = extended_imm << 2;
+    // Adding shifted immediate to i_npc_D to get o_branch_addr_D
+    assign o_branch_addr_D = shifted_imm + i_npc_D;
 
 endmodule
