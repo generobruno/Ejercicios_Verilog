605bef0f036b1eaad1b3b16afe353b2f0406b80c
Bruno Genero <generobruno@gmail.com>
2024-03-05 18:38:10 -0300

    Revisando BEQ

--- sources/PIPELINE_Test/PIPELINE/sims/pipeline_verbose_tb.v
+++ sources/PIPELINE_Test/PIPELINE/sims/pipeline_verbose_tb.v
@@ -251,7 +251,7 @@ module pipeline_verbose_tb();
         .i_alu_result_E(alu_result_EX_MEM), .i_operand_b_E(write_data_EX_MEM),
         .i_debug_addr(i_debug_addr),
         // Input Control Lines 
-        .i_mem_read_M(mem_read_EX_MEM), .i_mem_write_M(mem_write_EX_MEM), .i_bhw_M(bhw_EX_MEM),
+        .i_mem_write_M(mem_write_EX_MEM), .i_bhw_M(bhw_EX_MEM),
         // Outputs
         .o_alu_result_M(alu_result_M), .o_read_data_M(read_data_M), .o_debug_mem(o_mem)
         );
@@ -395,6 +395,7 @@ module pipeline_verbose_tb();
         reg_instr_rs = 5'b0000;
         reg_immediate = 16'h0002;
         reg_instr_rt = 5'b00010;
+        // reg[2] = 2
 
         i_instruction = {6'b001000, reg_instr_rs, reg_instr_rt, reg_immediate};
 
@@ -403,10 +404,11 @@ module pipeline_verbose_tb();
         //! SW - Store Test: mem[base+offset] <- rt
         // Inputs
         reg_base = 5'b00000;
-        reg_offset = 16'h0005;
+        reg_offset = 16'h0002;
         reg_instr_rt = 5'b00010;
+        // mem[0] = 2
 
-        i_debug_addr = 5'b00101; // GPR[rt] (read_data_1) + offset
+        i_debug_addr = 5'b00001; // Signed offset + GPR[base] // TODO Revisar y ver caso LH (y LB)
 
         i_instruction = {6'b101011, reg_base, reg_instr_rt, reg_offset};
 
@@ -415,10 +417,11 @@ module pipeline_verbose_tb();
         //! LW - Load Test: rt <- mem[base+offset]
         // Inputs
         reg_base = 5'b00000;
-        reg_offset = 16'h0005;
+        reg_offset = 16'h0002;
         reg_instr_rt = 5'b00101;
+        // reg[5] = 2
 
-        i_debug_addr = 5'b00010; // GPR[rt] (read_data_1) + offset
+        i_debug_addr = 5'b00010; // GPR[rt] (read_data_1) + offset // TODO Revisar
 
         i_instruction = {6'b100011, reg_base, reg_instr_rt, reg_offset};
 
@@ -429,11 +432,88 @@ module pipeline_verbose_tb();
         reg_instr_rs = 5'b00010;
         reg_instr_rt = 5'b00010;
         reg_instr_rd = 5'b01010;
+        // reg[10] = 4
 
         i_instruction = {6'b000000, reg_instr_rs, reg_instr_rt, reg_instr_rd, 5'b00000, ADDU};
 
         #(T*2);
 
+        //! SUBU - Sub Test: rd <- rs - rt
+        // Inputs
+        reg_instr_rs = 5'b01010;
+        reg_instr_rt = 5'b00010;
+        reg_instr_rd = 5'b00111;
+        // reg[7] = 2
+
+        i_instruction = {6'b000000, reg_instr_rs, reg_instr_rt, reg_instr_rd, 5'b00000, SUBU};
+
+        #(T*2);
+
+        //! AND - And Test: rd <- rs & rt
+        // Inputs
+        reg_instr_rs = 5'b00010;
+        reg_instr_rt = 5'b00010;
+        reg_instr_rd = 5'b01111;
+        // reg[15] = 2
+
+        i_instruction = {6'b000000, reg_instr_rs, reg_instr_rt, reg_instr_rd, 5'b00000, AND};
+
+        #(T*2);
+
+        //! ORI - Or Test: rt <- rs | imm
+        // Inputs
+        reg_instr_rs = 5'b0000;
+        reg_immediate = 16'h0002;
+        reg_instr_rt = 5'b11011;
+        // reg[27] = 2
+
+        i_instruction = {6'b001101, reg_instr_rs, reg_instr_rt, reg_immediate};
+
+        #(T*2);
+
+        //! Add To Test BEQ - 8th Inst (32'h20)
+        // Inputs
+        reg_instr_rs = 5'b00111;
+        reg_immediate = 16'h0002;
+        reg_instr_rt = 5'b00111;
+        // reg[7] += 2 = 4 (iter 1) -> 6 (iter 2)
+
+        i_instruction = {6'b001000, reg_instr_rs, reg_instr_rt, reg_immediate};
+        
+        #(T*2);
+
+        //! BEQ - Branch Test: rt <- rs | imm
+        // Inputs
+        reg_instr_rs = 5'b00111;
+        reg_offset = 16'h0002;//TODO VER
+        reg_instr_rt = 5'b01010;
+        // if reg[7] == reg[10] -> branch to
+
+        i_instruction = {6'b000100, reg_instr_rs, reg_instr_rt, reg_offset};
+
+        #(T*2);
+
+        //! Add To Test BEQ - 10th Inst (32'h28)
+        // Inputs
+        reg_instr_rs = 5'b00111;
+        reg_immediate = 16'h0002;
+        reg_instr_rt = 5'b00111;
+        // reg[7] += 2 = -> 6 
+
+        i_instruction = {6'b001000, reg_instr_rs, reg_instr_rt, reg_immediate};
+        
+        #(T*2);
+
+        //! Add To Test BEQ - 11th Inst (32'h2C)
+        // Inputs
+        reg_instr_rs = 5'b00111;
+        reg_immediate = 16'h0002;
+        reg_instr_rt = 5'b00111;
+        // reg[7] += 2 -> if BEQ 6 , else 8
+
+        i_instruction = {6'b001000, reg_instr_rs, reg_instr_rt, reg_immediate};
+        
+        #(T*2);
         //! HALT
         i_instruction = 32'b000000_00000_00000_00000_111111;
         
@@ -467,7 +547,7 @@ module pipeline_verbose_tb();
         begin
             i_debug_addr = i; // Address
             #(T*2);
-            $display("DEBUG REGS: %d from %b", o_reg, i_debug_addr[MEM_SZ-1:0]);
+            $display("DEBUG REGS: %d from %b (%d)", o_reg, i_debug_addr[4:0], i_debug_addr[4:0]);
         end
 
         $display("\nTESTS PASSED!");
